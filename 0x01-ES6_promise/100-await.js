c8127ecbe170ca3c87ae319f565fb50f95762cf6
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-23 21:46:09 +0300

    3-all.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,7 +1,13 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
+  let photo = null;
+  let user = null;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (err) {
+    console.log('Signup system offline');
+  }
   return { photo: photo || null, user: user || null };
 }
