bd589a349ef168bae5c504490b799bb2a5f5d98f
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-23 19:34:51 +0300

    ES6-Promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,5 @@
+# alx-frontend-javascript
+
+ALX Fronted Course
+
+0x01. ES6 Promises
