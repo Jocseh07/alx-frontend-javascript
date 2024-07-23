0a7fb3202c2e73e2488689e58b057773e0af23ae
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-23 19:37:37 +0300

    ES6-Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => {
-      return new Error();
-    });
+    .catch(() => new Error());
 }
