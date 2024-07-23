0f40ee1cd1070d5e2ab6e3ebc4fa40c9b61ebd51
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-23 21:30:08 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      if (
-        photoResponse.status === 'fulfilled'
-        && userResponse.status === 'fulfilled'
-      ) {
-        console.log(
-          photoResponse.value.body,
-          userResponse.value.firstName,
-          userResponse.value.lastName,
-        );
-      } else {
-        console.log('One or more promises rejected');
-      }
+  const allPromises = Promise.all([uploadPhoto(), createUser()]);
+  allPromises
+    .then(([Photo, User]) => {
+      console.log(Photo.body, User.firstName, User.lastName);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
