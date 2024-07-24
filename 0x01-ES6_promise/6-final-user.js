5c68f17d74c68f07a515307457d37160710b336e
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-24 22:01:36 +0300

    3-all.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,11 +9,9 @@ export default async function handleProfileSignup(
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
-    results.map((result) => ({
-      status: result.status,
-      value:
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value:
         result.status === 'fulfilled' ? result.value : String(result.reason),
-    }));
-  });
+  })));
 }
