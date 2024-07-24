11a5591892a77e0cdd3b7c6c691782ae4337f366
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-24 21:43:47 +0300

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let photo = null;
-  let user = null;
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      [photo, user] = values;
+      const [photo, user] = values;
       console.log(photo.body, user.firstName, user.lastName);
-      return {
-        body: photo.body,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      };
     })
     .catch(() => console.log('Signup system offline'));
 }
