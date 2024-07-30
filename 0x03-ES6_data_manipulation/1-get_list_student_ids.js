d4edae1ae4889edfde63d280cd5da6e951199f79
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-30 15:56:06 +0300

    $argv

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,7 @@
-export default function getListStudentsIds(list) {
+export default function getListStudentIds(list) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+
   return list.map((student) => student.id);
 }
