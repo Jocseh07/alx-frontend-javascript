0ab44d6b0a0a7ff47f45748be363ac1c90c5ace0
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-30 16:26:09 +0300

    $argv

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,11 @@ export default function createInt8TypedArray(length, index, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  if (index < 0 || index >= length) {
-    throw new Error('Index outside range');
+  try {
+    view.setInt8(index, value);
+  } catch (error) {
+    throw new Error('Position outside range');
   }
 
-  view.setInt8(index, value);
   return view;
 }
