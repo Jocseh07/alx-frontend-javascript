18665ca9f5e95d7b573e8a295071895be708ecf1
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-30 17:02:10 +0300

    $argv

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,8 @@
 export default function cleanSet(set, startString) {
   const result = [];
-  if (
-    startString.length === 0 ||
-    typeof startString !== 'string' ||
-    typeof set !== 'object'
-  ) {
-    return '';
-  }
+  if (typeof set !== 'object') return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
   for (const value of set) {
     if (value && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
