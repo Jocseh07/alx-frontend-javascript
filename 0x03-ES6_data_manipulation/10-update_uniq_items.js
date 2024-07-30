14b3e4bfbabe7227740a479bdf01c01fecbc76c0
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-30 17:22:03 +0300

    $argv

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,7 @@
 export default function updateUniqueItems(groceries) {
-  if (!(groceries instanceof Map)) {
-    throw new Error('Cannot process');
-  }
+  if (!(groceries instanceof Map)) throw new Error('Cannot process');
 
   groceries.forEach((value, key) => {
-    if (value === 1) {
-      groceries.set(key, 100);
-    }
+    if (value === 1) groceries.set(key, 100);
   });
 }
