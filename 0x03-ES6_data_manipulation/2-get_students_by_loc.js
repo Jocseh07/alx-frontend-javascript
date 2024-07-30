20469618143697b93fa8b6d9305b251c6f15bea2
Jocseh07 <josephmkaranja07@gmail.com>
2024-07-30 15:58:13 +0300

    $argv

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(list, location) {
+  if (!Array.isArray(list)) {
+    return [];
+  }
+
+  return list.filter((student) => student.location === location);
+}
